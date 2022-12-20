a91729c671e9706ebb4dd54d5f0000e49147bdc4
Yossef mohamed <47724276+YossefMohamed@users.noreply.github.com>
2022-12-20 19:02:02 +0200

    Delete contacting-service directory

--- contacting-service/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, Router } from "express";
-import Message from "../models/messageModel";
-
-const messageRouter = Router();
-import { getMessages, postMessage } from "../controllers/messageControllers";
-
-messageRouter.post("/:id", postMessage);
-messageRouter.get("/:id", getMessages);
-export default messageRouter;
