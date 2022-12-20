b21e3e64ec9e4d9d2f6a68bb6e1b1b5618fbeaf3
Yossef mohamed <47724276+YossefMohamed@users.noreply.github.com>
2022-12-20 19:02:18 +0200

    Delete property-service directory

--- property-service/src/routes/propertyRoutes/getProperty.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getProperty } from "../../controllers/propertyControllers";
-
-const router = Router();
-
-router.get("/:id", getProperty);
-
-export { router as getPropertyRouter };
--- property-service/src/routes/wishListRoutes/getUserWishList.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getUserWishList } from "../../controllers/wishListControllers";
-
-const router = Router();
-
-router.get("/", getUserWishList);
-
-export { router as getUserWishListRouter };
